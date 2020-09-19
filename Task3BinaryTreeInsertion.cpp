94ae54937aba822d5af5686da3616b8eb7f918e6
lipsolix <57266314+lipsolix@users.noreply.github.com>
2020-09-19 12:47:36 +0300

    some problem with countering of operations FIX NEEDED

--- Task3BinaryTreeInsertion.cpp
+++ Task3BinaryTreeInsertion.cpp
@@ -10,6 +10,14 @@ struct node {
     node* right = NULL;
 };
 
+
+void remove_binary_tree(node* node_ptr) {
+    if (node_ptr == NULL) return;
+    remove_binary_tree(node_ptr->left);
+    remove_binary_tree(node_ptr->right);
+    delete node_ptr;
+}
+
 int insert_to_binary_tree(int value, node* node_ptr) {
     int counter = 0;
     counter++;
@@ -32,15 +40,16 @@ int insert_to_binary_tree(int value, node* node_ptr) {
     return counter;
 }
 
-int* test_rand_tree(int* stats, int n) {
+int* test_rand_tree(int n) {
     node* root = new node;
     root->value = rand();
-
+    int* stats = new int[n];
     stats[0] += 1;
 
     for (int i = 1; i < n; i++) {
         stats[i] += insert_to_binary_tree(rand(), root);
     }
+    remove_binary_tree(root);
     return stats;
 }
 
@@ -54,18 +63,15 @@ int* test_ordered_tree(int n) {
     for (int i = 1; i < n; i++) {
         stats[i] = insert_to_binary_tree(i+1, root);
     }
+    remove_binary_tree(root);
     return stats;
 }
 
 void show_arr(int* arr, int n) {
     for (int i = 0; i < n; i++) 
-        printf("%i %i %f\n", i+1, arr[i], 1.0*arr[i]/(i+1));
+        printf("%i %i\n", i+1, arr[i]);
 }
 
-void show_arr(int* arr, int n, int step) {
-    for (int i = 0; i < n; i += step) 
-        printf("%i %i %f\n", i+1, arr[i], 1.0*arr[i]/(i+1));
-}
 
 void show_arr(double* arr, int n) {
     for (int i = 0; i < n; i++) 
@@ -81,24 +87,27 @@ int max_tree_depth(node* node_ptr) {
 }
 
 double* test_drive(int n, int iterations) {
-    int* total_stats = new int[n];
+    double* total_stats = new double[n];
     fill(total_stats, total_stats+n, 0);
-    double* results = new double[n];
     for (int i = 0; i < iterations; i++) {
-        int* stats = test_ordered_tree(n);
-        for (int i = 0; i < n; i++) {
-            total_stats[i] += stats[i];
+        //if ((i+1) % (iterations / 100) == 0) printf("%0.1f%\n", 100.0*(i+1)/iterations);
+        int* stats = test_rand_tree(n);
+        for (int j = 0; j < n; j++) {
+            total_stats[j] += stats[j];
         }
+        delete[] stats;
     }
+    
     for (int i = 0; i < n; i++) {
-        results[i] = 1.0*total_stats[i]/iterations;
+        total_stats[i] /= iterations;
     }
-    return results;
+    
+    return total_stats;
 }
 
 int main() {
     srand(time(0));
-    int n = 1000;
-    show_arr(test_drive(n, 100000), n);
+    int n = 20;
+    show_arr(test_drive(n, 1000), n);
     return 0;
 }
