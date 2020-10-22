7d466384ccec389b8596d811bdd6a03710092613
lipsolix <oligli@yandex.ru>
2020-10-22 22:49:37 +0300

    yet anothe commit

--- Task8.cpp
+++ Task8.cpp
@@ -3,6 +3,19 @@
 #include <ctime>
 using namespace std;
 
+unsigned long long counter;
+
+void C(int a = 1) {
+    counter += a;
+}
+
+inline void swap(int &a, int &b) {
+    C(3);
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
 struct Disjunctor {
     int first;
     int second;
@@ -12,6 +25,9 @@ struct Disjunctor {
     }
 
     Disjunctor(int a, int b) {
+        C(5);
+        if (a == b) 
+            a = 0;
         if (a > b) 
             swap(a, b);
         this->first = a;
@@ -19,21 +35,28 @@ struct Disjunctor {
     }
 
     bool isExceptional() {
+        C(2);
         if ((this->first + this->second) == 0)
             return true;
         return false;
     }
 
     bool isEqualTo(Disjunctor *d) {
+        C(2);
         if (this->first == d->first && this->second == d->second) return true;
+        C(2);
         if (this->first == d->second && this->second == d->first) return true;
         return false;
     }
 
     int getContraryDirection(Disjunctor *d) {
+        C(2);
         if (this->first == -d->first) return 1;
+        C(2);
         if (this->first == -d->second) return 2;
+        C(2);
         if (this->second == -d->first) return 3;
+        C(2);
         if (this->second == -d->second) return 4;
         return 0;
     }
@@ -41,17 +64,23 @@ struct Disjunctor {
 };
 
 bool isDuplicate(Disjunctor *disj, vector<Disjunctor*> &disjList) {
-    for (int i = 0; i < disjList.size(); i++) 
+    for (int i = 0; i < disjList.size(); i++) {
+        C();
         if (disj->isEqualTo(disjList[i])) 
             return true;
+    }
     return false;
 }
 
 Disjunctor* getResolvent(Disjunctor *a, Disjunctor *b) {
+    C();
     int contraryDirection = a->getContraryDirection(b);
+    C();
     if (contraryDirection == 0) 
         return 0;
+    C(2);
     int new_first, new_second;
+    C(6);
     switch (contraryDirection) {
         case 1: {
             new_first = a->second;
@@ -78,6 +107,7 @@ Disjunctor* getResolvent(Disjunctor *a, Disjunctor *b) {
             exit(-1);
         }
     }
+    C();
     return new Disjunctor(new_first, new_second);  
 }
 
@@ -89,38 +119,69 @@ void print(vector<Disjunctor*> &disjList) {
 }
 
 bool isResolvable(int leftIndex, int rightIndex, vector<Disjunctor*> &disjList) {
+    C();
     if (leftIndex > rightIndex) return true;
+    C();
     Disjunctor* tmpDisj;
     for (int elderItemIndex = rightIndex; leftIndex <= elderItemIndex; elderItemIndex--) 
         for (int youngerItemIndex = elderItemIndex - 1; 0 <= youngerItemIndex; youngerItemIndex--) {
+            C();
             tmpDisj = getResolvent(disjList[elderItemIndex], disjList[youngerItemIndex]);
-            if (tmpDisj == 0) 
+            C();
+            if (tmpDisj == 0) {
+                delete tmpDisj;
                 continue; 
+            }
+            C();
             if (tmpDisj->isExceptional()) 
                 return false;
-            if (isDuplicate(tmpDisj, disjList)) 
+            C();
+            if (isDuplicate(tmpDisj, disjList)) { 
+                delete tmpDisj;
                 continue;
+            }
+            C();
             disjList.push_back(tmpDisj);
         }
-    print(disjList);
+    //print(disjList);
     return isResolvable(rightIndex + 1, disjList.size() - 1, disjList);
 }
 
-Disjunctor* getRandDisj(int max_value) {
-    
+Disjunctor* getRandDisj(int maxValue) {
+    int a, b;
+    do {
+        a = (rand() % (2 * maxValue) + 1) - maxValue; 
+        b = (rand() % (2 * maxValue) + 1) - maxValue; 
+    } while ((a + b) == 0);
+    return new Disjunctor(a, b);
+}
+
+void addDisj(int a, int b, vector<Disjunctor*> &disjList) {
+    disjList.push_back(new Disjunctor(a, b));
+}
+
+void addNUniqDisjToList(vector<Disjunctor*> &disjList, int disjCount, int maxValue) {
+    for (int i = 0; i < disjCount; i++) {
+        Disjunctor* tmp = 0;
+        do {
+            tmp = getRandDisj(maxValue);
+        } while (isDuplicate(tmp, disjList) || tmp->isExceptional());
+        disjList.push_back(tmp);
+    }
+}
+
+int doTest(int disjCount, int maxLiteralIndex) {
+    counter = 0;
+    vector <Disjunctor*> v;
+    addNUniqDisjToList(v, disjCount, 1000);
+    isResolvable(0, disjCount - 1, v);
+    return counter;
 }
 
 
 int main() {
     srand(time(0));
-    vector <Disjunctor*> v;
-    v.push_back(new Disjunctor(0, 5));
-    v.push_back(new Disjunctor(1, 2));
-    v.push_back(new Disjunctor(-2, 0));
-
-    print(v);
-    cout << isResolvable(0, 2, v) << endl;
-    //print(v);
+    cout << doTest(10000, 10000) << endl;
 
 
     return 0;
