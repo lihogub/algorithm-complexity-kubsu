e7ad6aeb063c333020a53443d81ca0df86421f05
lipsolix <oligli@yandex.ru>
2020-10-30 15:10:18 +0300

    some code

--- Task9.cpp
+++ Task9.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <math.h>
+#include <vector>
 using namespace std;
-typedef unsigned long long cypher;
+typedef u_int64_t ULL;
 
 bool numbers[1000000] = {0};
 int primes[1000000] = {0};
+
 void generatePrimes(int a, int b) {
     for (int i = 2; i < b; i++) {
-        for (int m = max(a/i, 2); i*m < b; m++) {
-            numbers[i*m] = 1;
+        for (int m = max(a/i, 2); m*i < b; m++) {
+            numbers[m*i] = 1;
         }
     }
     primes[1] = 1;
@@ -19,13 +22,6 @@ void generatePrimes(int a, int b) {
         }
     }
 
-}
-
-cypher pow(cypher text, int k) {
-    cypher tmp = text;
-    for (int i = 0; i < k; i++) 
-        text *= tmp;
-    return text;
 }
 
 void print(int a, int b) {
@@ -40,65 +36,106 @@ void printPrimes() {
     }
 }
 
-int getPrime(int i) {
+ULL getPrime(int i) {
     return primes[i];
 }
 
 
-cypher stringToULL(char* s) {
-    return *(cypher*)s;
+ULL stringToULL(char* s) {
+    return *(ULL*)s;
 }
 
-int getK(int a, int b) {
-    return (a-1)*(b-1);
-}
-
-int GCD(int a, int b) {
+ULL GCD(ULL a, ULL b) {
     return !b ? a : GCD(b , a%b);
 }
 
-int getMutualSimpleByModulo(int number, int modulo) {
-    for (int i = 2; i < number; i++) {
-        if (GCD(number, i) % modulo == 1) return i;
+ULL getK(ULL a, ULL b) {
+    return (a-1)*(b-1);
+}
+ULL fastModExp(ULL a, ULL k, ULL modulus) {
+    ULL result = 1;
+    ULL temp;
+    if (k > 0) { 
+        if ((k % 2) == 0) {
+            temp = fastModExp(a,k/2,modulus);
+            result = (temp * temp) % modulus;
+        }
+        else {
+            temp = fastModExp(a,k-1,modulus);
+            result = (a * temp) % modulus;
+        }
     }
-    return -1;
+    return result;
 }
 
-int getMutualSimple(int number) {
-    for (int i = 2; i < number; i++) {
-        if (GCD(number, i) == 1) return i;
-    }
-    return -1;
+ULL getN(ULL a, ULL b) {
+    return a*b;
 }
 
-cypher encrypt(cypher msg, int pub, int k) {
-    return pow(msg, pub) % k;
+ULL getPublicKey(ULL k) {
+    for (ULL publicKey = 2; publicKey < k; publicKey++)
+        if (GCD(publicKey, k) == 1) 
+            return publicKey;
+    return 0;
 }
 
-cypher decrypt(cypher msg, int close, int k) {
-    return pow(msg, close) % k;
+ULL getPrivateKey(ULL publicKey, ULL k) {
+    return fastModExp(publicKey, k-2, k);
+}
+
+ULL encrypt(ULL plainText, ULL openKey, int n) {
+    return fastModExp(plainText, openKey, n);
+}
+
+ULL decrypt(ULL cypherText, ULL closeKey, int n) {
+    return fastModExp(cypherText, closeKey, n);
 }
 
 int main() {
-    generatePrimes(1, 1000);
+
+    generatePrimes(1, 200000);
     //printPrimes();
-    int a = getPrime(5);
-    int b = getPrime(10);
 
-    int k = getK(a, b);
-    int pub = getMutualSimple(k);
-    int close = getMutualSimpleByModulo(pub, k);
-    
-    cout << pub << endl;
-    cout << close << endl;
 
-/*
-    char buf[16] = {0};
-    cin >> buf;
-    */
-    cypher enc = encrypt(0, pub, k);
-    cout << enc << endl;
-    cout << decrypt(enc, close, k);
+    ULL a = getPrime(10);
+    cout << "a " << a << endl;
+
+    ULL b = getPrime(11);
+    cout << "b " << b << endl;
+
+    ULL n = getN(a, b);
+    cout << "n " << n << endl;
+
+    ULL k = getK(a, b);
+    cout << "k " << k << endl;
+
+    ULL open = getPublicKey(k);
+    cout << "open " << open << endl;
+
+    ULL close = getPrivateKey(open, k);
+    cout << "close " << close << endl;
+
+    /*
+    char buf[8] = {0};
+    buf[0] = 'a';
+    buf[1] = 'a';
+
+    ULL plainText = stringToULL(buf);
+    cout << plainText << endl;
+*/
+
+
+    ULL plainText = 123;
+    ULL cypherText = encrypt(plainText, open, n);
+    cout << cypherText << endl;
+    plainText = decrypt(cypherText, close, n);
+    cout << plainText << endl;
+
+
+
+    //cout << fastPow(545, 503, 943) << endl;
+
+
 
     return 0;
 }
